6e796ed0a78661f7eba8adcd47078a4e727ea039
agustinchavez <chavez13@illinois.edu>
2016-01-22 17:19:21 -0600

    fixed bug with routing to views

--- app/controllers/menu_items_controller.rb
+++ app/controllers/menu_items_controller.rb
@@ -3,78 +3,33 @@ class MenuItemsController < ApplicationController
   before_action :find_menu_item, :authenticate_restaurant
   before_action :authorize_restaurant, {except: :create}
 
-  # GET /menu_items
-  # GET /menu_items.json
-  def index
-    @menu_items = MenuItem.all
-  end
-
-  # GET /menu_items/1
-  # GET /menu_items/1.json
-  def show
-  end
-
-  # GET /menu_items/new
-  def new
-    @menu_item = MenuItem.new
-  end
-
-  # GET /menu_items/1/edit
-  def edit
-  end
-
-  # POST /menu_items
-  # POST /menu_items.json
-  def create
+ def create
     restaurant = current_restaurant
-
-    respond_to do |format|
-      if @menu_item.save
-        format.html { redirect_to @menu_item, notice: 'Menu item was successfully created.' }
-        format.json { render :show, status: :created, location: @menu_item }
-      else
-        format.html { render :new }
-        format.json { render json: @menu_item.errors, status: :unprocessable_entity }
-      end
-    end
+    menu_item = restaurant.menu_items.build(menu_item_params)
+    menu_item.save
+    redirect_to restaurants_profile_path
   end
 
-  # PATCH/PUT /menu_items/1
-  # PATCH/PUT /menu_items/1.json
   def update
     @restaurant = @menu_item.restaurant
     @menu_item.update_attributes(menu_item_params)
-    respond_to do |format|
-      if @menu_item.update(menu_item_params)
-        format.html { redirect_to @menu_item, notice: 'Menu item was successfully updated.' }
-        format.json { render :show, status: :ok, location: @menu_item }
-      else
-        format.html { render :edit }
-        format.json { render json: @menu_item.errors, status: :unprocessable_entity }
-      end
-    end
+    redirect_to restaurants_profile_path
   end
 
-  # DELETE /menu_items/1
-  # DELETE /menu_items/1.json
   def destroy
     @restaurant = @menu_item.restaurant
     @menu_item.destroy
-    respond_to do |format|
-      format.html { redirect_to menu_items_url, notice: 'Menu item was successfully destroyed.' }
-      format.json { head :no_content }
-    end
+    redirect_to restaurants_profile_path
   end
 
-  private
-    # Use callbacks to share common setup or constraints between actions.
-  def find_menu_item
-    @menu_item = MenuItem.find_by_slug(params[:id])
-  end
+ private
 
-    # Never trust parameters from the scary internet, only allow the white list through.
   def menu_item_params
     params.require(:menu_item).permit(:name, :price)
+  end
+
+  def find_menu_item
+    @menu_item = MenuItem.find_by_slug(params[:id])
   end
 
   def authenticate_restaurant
